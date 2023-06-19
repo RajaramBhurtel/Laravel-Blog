028614105a500a7142b5010897d571afec9f137f
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-06-19 13:43:28 +0545

    pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,7 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
            @include('template.post-grid',  $posts)
+           {{ $posts->links()}}
         @else
             <p class="text-center">No Posts found. Please come back later.</p>
         @endif
