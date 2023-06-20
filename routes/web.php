3d29e0fb55a534ed28f33f9e2da9b74b6bed4882
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-06-20 15:54:00 +0545

    logout or session destroy

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegistrationController;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -14,5 +12,6 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get( '/' , [PostController::class, 'index'])->name('home');
 Route::get( 'posts/{post:slug}' , [PostController::class, 'show']);
 
-Route::get( 'register' , [RegistrationController::class, 'create']);
-Route::post( 'register' , [RegistrationController::class, 'store']);
+Route::get( 'register' , [RegistrationController::class, 'create'])->middleware('guest');
+Route::post( 'register' , [RegistrationController::class, 'store'])->middleware('guest');
+Route::post( 'logout' , [SessionController::class, 'destroy']);
