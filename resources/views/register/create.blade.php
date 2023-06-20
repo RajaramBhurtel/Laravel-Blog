242ed5f025329b224e8dac234c7b3d85649fb743
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-06-20 14:16:08 +0545

    validation error message

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,8 +16,12 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name')}}"
                            required
                     >
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -31,8 +35,12 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username')}}"
                            required
                     >
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -46,8 +54,12 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email')}}"
                            required
                     >
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -63,6 +75,9 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
