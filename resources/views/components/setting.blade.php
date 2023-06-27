cc81d558626fb0a0f331d1883494819b795d8d3c
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-06-27 14:01:06 +0545

    setting components

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -1,9 +1,24 @@
 @include('template.header')
-<section class="py-8 max-w-md mx-auto">
-    <h1 class="text-lg font-bold mb-4">{{ $heading }}</h1>
+<section class="py-8 max-w-4xl mx-auto">
+    <h1 class="text-lg font-bold mb-8 pb-2 border-b">{{ $heading }}</h1>
     
-    <div class="border border-gray-200 p-6 rounded-xl ">
-        {{ $slot }}
+    <div class="flex">
+        <aside class="w-48">
+            <h4 class="font-semibold mb-4">Links</h4>
+            <ul>
+                <li>
+                    <a href="/admin/dashboard" class="{{ request()->is('admin/dashboard') ? 'text-blue-500' : ''}}">Dashboard</a>
+                </li>
+                <li>
+                    <a href="/admin/posts/create" class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : ''}}">New Post</a>
+                </li>
+            </ul>
+        </aside>
+        <main class="flex-1">
+            <div class="border border-gray-200 p-6 rounded-xl ">
+                {{ $slot }}
+            </div>
+        </main>
     </div>
 </section>
 
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,139 +1,3 @@
-{{-- @include('template.header')
-<section class="py-8 max-w-md mx-auto">
-    <h1 class="text-lg font-bold mb-4">Publish New Post</h1>
-    <div class="border border-gray-200 p-6 rounded-xl ">
-        <form method="POST" action="/admin/posts" enctype="multipart/form-data">
-            @csrf
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="title"
-                >
-                    Title
-                </label>
-
-                <input class="border border-gray-400 p-2 w-full"
-                       type="text"
-                       name="title"
-                       id="title"
-                       value="{{ old('title') }}"
-                       required
-                >
-
-                @error('title')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="slug"
-                >
-                    Slug
-                </label>
-
-                <input class="border border-gray-400 p-2 w-full"
-                       type="text"
-                       name="slug"
-                       id="slug"
-                       value="{{ old('slug') }}"
-                       required
-                >
-
-                @error('slug')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="excerpt"
-                >
-                    Excerpt
-                </label>
-
-                <textarea class="border border-gray-400 p-2 w-full"
-                       name="excerpt"
-                       id="excerpt"
-                       required
-                >{{ old('excerpt') }}</textarea>
-
-                @error('excerpt')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="thumbnail"
-                >
-                    Thumbnail
-                </label>
-
-                <input class="border border-gray-400 p-2 w-full"
-                       type="file"
-                       name="thumbnail"
-                       id="thumbnail"
-                       required
-                >
-
-                @error('thumbnail')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="body"
-                >
-                    Body
-                </label>
-
-                <textarea class="border border-gray-400 p-2 w-full"
-                       name="body"
-                       id="body"
-                       required
-                >{{ old('body') }}</textarea>
-
-                @error('body')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <div class="mb-6">
-                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                       for="category_id"
-                >
-                    Category
-                </label>
-
-                <select name="category_id" id="category_id">
-                    @foreach (\App\Models\Category::all() as $category)
-                        <option
-                            value="{{ $category->id }}"
-                            {{ old('category_id') == $category->id ? 'selected' : '' }}
-                        >{{ ucwords($category->name) }}</option>
-                    @endforeach
-                </select>
-
-                @error('category')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                @enderror
-            </div>
-            <div class="mb-6">
-                <button type="submit"
-                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                >
-                Publish
-                </button>
-            </div>
-        </form>
-    </div>
-</section>
-
-@include('template.footer') --}}
-
 <x-setting heading="Create New Post">
     <form method="POST" action="/admin/posts" enctype="multipart/form-data">
         @csrf
@@ -145,7 +9,7 @@
                 Title
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
+            <input class="border border-gray-200 rounded p-2 w-full"
                    type="text"
                    name="title"
                    id="title"
@@ -165,7 +29,7 @@
                 Slug
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
+            <input class="border border-gray-200 rounded p-2 w-full"
                    type="text"
                    name="slug"
                    id="slug"
@@ -186,7 +50,7 @@
                 Excerpt
             </label>
 
-            <textarea class="border border-gray-400 p-2 w-full"
+            <textarea class="border border-gray-200 rounded p-2 w-full"
                    name="excerpt"
                    id="excerpt"
                    required
@@ -204,7 +68,7 @@
                 Thumbnail
             </label>
 
-            <input class="border border-gray-400 p-2 w-full"
+            <input class="border border-gray-200 rounded p-2 w-full"
                    type="file"
                    name="thumbnail"
                    id="thumbnail"
@@ -223,7 +87,7 @@
                 Body
             </label>
 
-            <textarea class="border border-gray-400 p-2 w-full"
+            <textarea class="border border-gray-200 rounded p-2 w-full"
                    name="body"
                    id="body"
                    required
